239b589af893f78cac15f6691cc7efbcb769cfa9
Jitendra <jyjitendrayadav105@gmail.com>
2024-01-05 17:52:58 +0530

    files uploaded

--- src/pages/reels/ReelsData.js
+++ src/pages/reels/ReelsData.js
@@ -2,18 +2,8 @@ import dance from "../../assets/reels/dance.mp4"
 import beach from "../../assets/reels/beach.mp4"
 import bridge from "../../assets/reels/bridge.mp4"
 import celebrations from "../../assets/reels/celebrations.mp4"
-// import funny1 from "../../assets/reels/funny1.mp4"
-// import funny2 from "../../assets/reels/funny2.mp4"
-// import funny3 from "../../assets/reels/funny3.mp4"
-// import funny4 from "../../assets/reels/funny4.mp4"
-// import horses from "../../assets/reels/horses.mp4"
-// import music from "../../assets/reels/music.mp4"
-// import nightlife from "../../assets/reels/nightlife.mp4"
-// import roadside from "../../assets/reels/roadside.mp4"
 import romantic from "../../assets/reels/romantic.mp4"
-// import romantic2 from "../../assets/reels/romantic2.mp4"
-// import snow from "../../assets/reels/snow.mp4"
-// import watchtower from "../../assets/reels/watchtower.mp4"
+
 
 const ReelsData = [
     {
@@ -48,70 +38,6 @@ const ReelsData = [
         rlikes: "2.3k",
         rcomments: "200",
     },
-    // {
-    //     video: funny1,
-    //     rname: "LoL",
-    //     rfollowedBy: "2.3k",
-    //     rfollowBtn: "follow",
-    //     rlikes: "2.3k",
-    //     rcomments: "200",
-    // },
-    // {
-    //     video: funny2,
-    //     rname: "LoL",
-    //     rfollowedBy: "2.3k",
-    //     rfollowBtn: "follow",
-    //     rlikes: "2.3k",
-    //     rcomments: "200",
-    // },
-    // {
-    //     video: funny3,
-    //     rname: "LoL",
-    //     rfollowedBy: "2.3k",
-    //     rfollowBtn: "follow",
-    //     rlikes: "2.3k",
-    //     rcomments: "200",
-    // },
-    // {
-    //     video: funny4,
-    //     rname: "LoL",
-    //     rfollowedBy: "2.3k",
-    //     rfollowBtn: "follow",
-    //     rlikes: "2.3k",
-    //     rcomments: "200",
-    // },
-    // {
-    //     video: horses,
-    //     rname: "Animals",
-    //     rfollowedBy: "2.3k",
-    //     rfollowBtn: "follow",
-    //     rlikes: "2.3k",
-    //     rcomments: "200",
-    // },
-    // {
-    //     video: music,
-    //     rname: "YO YO",
-    //     rfollowedBy: "2.3k",
-    //     rfollowBtn: "follow",
-    //     rlikes: "2.3k",
-    //     rcomments: "200",
-    // },
-    // {
-    //     video: nightlife,
-    //     rname: "culture",
-    //     rfollowedBy: "2.3k",
-    //     rfollowBtn: "follow",
-    //     rlikes: "2.3k",
-    //     rcomments: "200",
-    // },
-    // {
-    //     video: roadside,
-    //     rname: "culture",
-    //     rfollowedBy: "2.3k",
-    //     rfollowBtn: "follow",
-    //     rlikes: "2.3k",
-    //     rcomments: "200",
-    // },
     {
         video: romantic,
         rname: "Nibba & Nibbi",
@@ -120,30 +46,7 @@ const ReelsData = [
         rlikes: "2.3k",
         rcomments: "200",
     },
-    // {
-    //     video: romantic2,
-    //     rname: "Nibba & Nibbi",
-    //     rfollowedBy: "2.3k",
-    //     rfollowBtn: "follow",
-    //     rlikes: "2.3k",
-    //     rcomments: "200",
-    // },
-    // {
-    //     video: snow,
-    //     rname: "weather",
-    //     rfollowedBy: "2.3k",
-    //     rfollowBtn: "follow",
-    //     rlikes: "2.3k",
-    //     rcomments: "200",
-    // },
-    // {
-    //     video: watchtower,
-    //     rname: "culture",
-    //     rfollowedBy: "2.3k",
-    //     rfollowBtn: "follow",
-    //     rlikes: "2.3k",
-    //     rcomments: "200",
-    // },
+
 ]
 
 export default ReelsData;
